ddb33078aef56f16d6d44d035280f6c8f52563f2
Depei Zhu <hyekyoz@outlook.com>
2017-12-15 14:37:06 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,61 +6,74 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-admin = User.new(
-  email: "hyekyoz@outlook.com",
-  password: "password123"
-)
-admin.skip_confirmation!
-admin.save
-admin.add_role :admin
+def users_seed(size = 30)
+  admin = User.new(
+    email: "hyekyoz@outlook.com",
+    password: "password123"
+  )
+  admin.skip_confirmation!
+  admin.save
+  admin.add_role :admin
 
-merchant = User.new(
-  email: "799654910@qq.com",
-  password: "password123"
-)
-merchant.skip_confirmation!
-merchant.save
-merchant.add_role :merchant
+  merchant = User.new(
+    email: "799654910@qq.com",
+    password: "password123"
+  )
+  merchant.skip_confirmation!
+  merchant.save
+  merchant.add_role :merchant
 
-def picture
+  size.times do |index|
+    created_time = Time.now - (rand(1..1000)).day
+    user = User.new(
+      email: "#{Faker::Name.unique.first_name}@jforder.com",
+      password: "password123",
+      created_at: created_time,
+      updated_at: created_time
+    )
+    user.skip_confirmation!
+    user.save
+    user.add_role :merchant
+  end
+end
+
+def random_picture
   pictures_url = ENV['CODE_PICTURES']
   entries = Dir.new(pictures_url).entries
   entries.delete_if { |entry| entry =~ /^\./ }
   "#{pictures_url}/#{entries.sample}"
 end
 
-30.times do |index|
-  created_time = Time.now - (rand(1..1000)).day
-  user = User.new(
-    email: "#{Faker::Name.unique.first_name}@jforder.com",
-    password: "password123",
-    created_at: created_time,
-    updated_at: created_time
-  )
-  user.skip_confirmation!
-  user.save
-  user.add_role :merchant
-  user.stores.create(
-    name: Faker::Company.name,
-    intro: Faker::Address.street_address
-  )
-end
-
-Store.all.each do |store|
-  File.open(picture) do |f|
-    store.build_picture(picture: f)
+# size 是每个用户的商铺数量
+def stores_seed(size = 2)
+  User.all.each do |user|
+    size.times { user.stores.create name: Faker::Company.name, intro: Faker::Address.street_address }
   end
-  store.save
-end
 
-User.all.each do |user|
-  unless user.id == 1
-    user.stores.each do |store|
-      store.menus.new(
-        name: Faker::Food.dish,
-        intro: Faker::Lorem.sentence,
-        price: rand(100..9999)
-      )
+  Store.all.each do |store|
+    File.open(random_picture) do |f|
+      store.create_picture picture: f
     end
   end
 end
+
+# size 是每个商铺的菜品数量
+def menus_seed(size = 6)
+  User.all.each do |user|
+    unless user.id == 1
+      user.stores.each do |store|
+        3.times { store.menus.create name: Faker::Food.dish, intro: Faker::Lorem.sentence, price: rand(100..9999) }
+      end
+    end
+  end
+
+  Menu.all.each do |menu|
+    File.open(random_picture) do |f|
+      3.times { menu.pictures.create picture: f }
+    end
+  end
+end
+
+users_seed
+stores_seed
+menus_seed
