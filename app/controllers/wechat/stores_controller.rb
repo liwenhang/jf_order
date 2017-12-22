f480a59a0b67c56ec921b347c69b5b432f85c91d
Depei Zhu <hyekyoz@outlook.com>
2017-12-22 11:09:47 +0800

    修复 server 和 client 购物车信息统一问题

--- app/controllers/wechat/stores_controller.rb
+++ app/controllers/wechat/stores_controller.rb
@@ -8,5 +8,9 @@ class Wechat::StoresController < Wechat::BaseController
     @store = Store.find params[:id]
     store_cart = "store#{@store.id}"
     @cart = Cart.from_hash(session[store_cart.to_sym])
+
+    # 删除客服端里的 session 仍然保存着的商家删除或下线的菜品
+    @cart.items.delete_if { |item| !item.menu.publish? || item.menu.nil? }
+    session[store_cart.to_sym] = @cart.serialize
   end
 end
